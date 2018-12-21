eccb42156c6a3b674c6253c5e39eb46cf05e10fb
EasyAI Chen <easyai@outlook.com>
2018-12-21 11:35:20 +0800

    rewrite intr

--- src/kernel/intr/intr.c
+++ src/kernel/intr/intr.c
@@ -1,37 +1,83 @@
+/*
+  Copyright 2018 EmmmHackers
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+  --------------------------
+  File: mm.h
+  Project: EmmmCS
+  File Created: 2018-12-21 10:17:05
+  Author: Chen Haodong (easyai@outlook.com)
+          Xie Nairong (jujianai@hotmail.com)
+  --------------------------
+  Last Modified: 2018-12-21 10:58:36
+  Modified By: Chen Haodong (easyai@outlook.com)
+ */
 #include "intr.h"
-#include "../driver/kbd.h"
 #include "../riscv_asm.h"
 
-static u8* intr_args = (u8*)INDR_ADDR;
+static void *intr_handlers[INTR_COUNT];
 
 void intr_init(){
-    *((u32*) intr_args) = 0;
-    void* intr_ptr = intr;
-    write_csr(uscratch, intr_ptr); // fake instr
-    // asm volatile("csrw uscratch, %0": "r"(intr_ptr));
+    write_csr(uscratch, intr_handlers);
 }
 
-void intr_open(){
-    int code = 1;
-    write_csr(uepc, code); // fake instr
-    // asm volatile("csrw uepc, %0": "r"(code));
+void intr_handler_register(u8 intrno, void *handler)
+{
+    if(intrno < INTR_COUNT)
+        intr_handlers[intrno] = handler;
 }
 
-void intr_close(){
-    int code = 0;
-    write_csr(uepc, code); // fake instr
-    // asm volatile("csrw uepc, %0": "r"(code));
+void intr_on(){
+    write_csr(uepc, 1);
 }
+void intr_off(){
+    write_csr(uepc, 0);
+}
+void intr(u8 intrno){
+    if(intrno < INTR_COUNT)
+        (*(void (*)(void))(&intr_handlers[intrno]))();
+    write_csr(ucause, 0);
+}
+// static u8* intr_args = (u8*)INDR_ADDR;
 
-void intr(){
-    switch(intr_args[0]){
-        case 0: return;
-        case 1: kbd_update(intr_args + 1); break;
-        default: break;
-    }
-    intr_close();
-    int code = 0;
-    write_csr(ucause, code); // fake MRET instr
-    // asm volatile("csrw ucause, %0": "r"(code));
-    return;
-}
+// void intr_init(){
+//     *((u32*) intr_args) = 0;
+//     void* intr_ptr = intr;
+//     write_csr(uscratch, intr_ptr); // fake instr
+//     // asm volatile("csrw uscratch, %0": "r"(intr_ptr));
+// }
+
+// void intr_open(){
+//     int code = 1;
+//     write_csr(uepc, code); // fake instr
+//     // asm volatile("csrw uepc, %0": "r"(code));
+// }
+
+// void intr_close(){
+//     int code = 0;
+//     write_csr(uepc, code); // fake instr
+//     // asm volatile("csrw uepc, %0": "r"(code));
+// }
+
+// void intr(){
+//     switch(intr_args[0]){
+//         case 0: return;
+//         case 1: kbd_update(intr_args + 1); break;
+//         default: break;
+//     }
+//     intr_close();
+//     int code = 0;
+//     write_csr(ucause, code); // fake MRET instr
+//     // asm volatile("csrw ucause, %0": "r"(code));
+//     return;
+// }
--- src/kernel/intr/intr.h
+++ src/kernel/intr/intr.h
@@ -1,13 +1,41 @@
-#ifndef KLIB_INTR_H
-#define KLIB_INTR_H
+/*
+  Copyright 2018 EmmmHackers
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+  --------------------------
+  File: mm.h
+  Project: EmmmCS
+  File Created: 2018-12-21 10:17:05
+  Author: Chen Haodong (easyai@outlook.com)
+          Xie Nairong (jujianai@hotmail.com)
+  --------------------------
+  Last Modified: 2018-12-21 10:44:41
+  Modified By: Chen Haodong (easyai@outlook.com)
+ */
+#ifndef KERNEL_INTR_H
+#define KERNEL_INTR_H
 
 #include "../typedef.h"
 
-#define INDR_ADDR 0x7fffc
+#define INTR_COUNT 3
+#define FATAL_ERROR 0x0
+#define KBD_INTR 0x1
+#define TIMER_INTR 0x2
+//#define INDR_ADDR 0x7fffc
 
-void intr_init();
-void intr_open();
-void intr_close();
-void intr();
+//void intr_init();
+//void intr_open();
+//void intr_close();
+//void intr();
 
 #endif
