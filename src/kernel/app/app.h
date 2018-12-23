b20839b7e38068e80e00cca310911857b904928a
bluebread <hotbread70127@gmail.com>
2018-12-23 20:03:16 +0800

    fix eval dealloc error

--- src/kernel/app/app.h
+++ src/kernel/app/app.h
@@ -1,13 +1,13 @@
 #ifndef __KLIBAPP_H__
 #define __KLIBAPP_H__
 
-#define STD_DEBUG
+//#define STD_DEBUG
 
 #include "../klib/string.h"
 
 #ifndef STD_DEBUG
 #include "../mm/mm.h"
-#include "../klib/stdio.h"
+//#include "../klib/stdio.h"
 #endif // STD_DEBUG
 
 int eval(const char* express);
