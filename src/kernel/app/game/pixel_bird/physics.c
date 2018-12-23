2e5e264954f17f99f68dbff4316ed9700f816fe8
EasyAI Chen <easyai@outlook.com>
2018-12-23 20:50:52 +0800

    updated pixel bird

--- src/kernel/app/game/pixel_bird/physics.c
+++ src/kernel/app/game/pixel_bird/physics.c
@@ -18,18 +18,25 @@
   File Created: 2018-12-22 20:30:41
   Author: Chen Haodong (easyai@outlook.com)
   --------------------------
-  Last Modified: 2018-12-22 21:28:49
+  Last Modified: 2018-12-23 20:45:05
   Modified By: Chen Haodong (easyai@outlook.com)
  */
 
 #include "physics.h"
-
-pb_rigid_body_t *pb_rigid_body_create(u8 x_s, u8 y_s, sprite_t *spr)
+static pb_rigid_body_t rigid_bodys[9];
+void pb_rigid_body_create(u8 idx, u8 x_s, u8 y_s, u8 w, u8 l, s8 x, s8 y, u8 p_w, u8 p_l, s8 p_x, s8 p_y)
 {
-    pb_rigid_body_t *body = (pb_rigid_body_t *)malloc(sizeof(pb_rigid_body_t));
+    pb_rigid_body_t *body = &rigid_bodys[idx];
     body->x_speed = x_s;
     body->y_speed = y_s;
-    body->spr = spr;
+    body->spr.width = w;
+    body->spr.length = l;
+    body->spr.x = x;
+    body->spr.y = y;
+    body->spr.phy_width = p_w;
+    body->spr.phy_length = p_l;
+    body->spr.phy_x = p_x;
+    body->spr.phy_y = p_y;
 }
 void pb_rigid_body_destroy(pb_rigid_body_t *body)
 {
